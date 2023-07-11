e4eb763999977e68c09e80c0ce31d234ab0c2ec0
Syed Zubair <syedzubi270@gmail.com>
2023-07-11 17:27:07 -0400

    hangman completed

---
+++ logic.py
@@ -0,0 +1,53 @@
+#Step 5
+
+import random
+from hangman_words import word_list
+from hangman_art import logo,stages
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(logo)
+
+#Testing code
+# 
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+      print(f"{guess} letter has already been guessed , choose another letter")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"{guess} letter is not present ")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+            print(f'The correct word was :{chosen_word}.')
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
